2d91927235832998285f573a5830df67c88e1e94
krinali123 <paneliyakrinali2@gmail.com>
2022-04-27 10:14:00 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //  const x =10; //global scope
+  //  {
+  //    const x =20; //local scope
+  //  }
+  //  console.log(x);
+
+  //  let y = 5;
+  //  y=y+2;
+  //  console.log(y);
+
+  //  const z = 5;
+  //  z=z+2;
+  //  console.log(z); //error
+  
+  // const a;
+  // a=50; //error
+
+  // let b;
+  // b=50;
+  // console.log(b);
+   
+
+  // arr function method
+
+  // let arr = [10,20,30,40,50];
+  // arr.map((a,i) => console.log(a));
+  
+  let intro = () => {
+    console.log("paneliya krinali");
+  }
+  intro();
+
   return (
     <div className="App">
       <header className="App-header">

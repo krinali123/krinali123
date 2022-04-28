5bc998f96cc81e643f499fa2a7311a29fc146516
kartik5800 <kartiknavadiya198@gmail.com>
2022-04-28 10:12:35 +0530

    28_4

--- src/App.js
+++ src/App.js
@@ -2,62 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //  const x =10; //global scope
-  //  {
-  //    const x =20; //local scope
-  //  }
-  //  console.log(x);
-
-  //  let y = 5;
-  //  y=y+2;
-  //  console.log(y);
-
-  //  const z = 5;
-  //  z=z+2;
-  //  console.log(z); //error
+          //  let arr=[10,20,30,40,50];
+          // //  console.log(arr[4]);
+          //  let obj={
+          //    id:10,
+          //    age:20,
+          //    per:30
+          //  }  
+          //  console.log(obj["id"]);           
+       
+          // let arr=[10,20,30,40,50];
+          // arr.push(99);
+          // console.log(arr);
   
-  // const a;
-  // a=50; //error
-
-  // let b;
-  // b=50;
-  // console.log(b);
-   
-
-  // arr function method
-
-  // let arr = [10,20,30,40,50];
-  // arr.map((a,i) => console.log(a));
-  
-  // let intro = () => {
-  //   console.log("paneliya krinali");
-  // }
-  // intro();
+          // let arr=[10,20,30,40,50];
+          // arr.unshift(99);
+          // console.log(arr);
 
 
-// 1 is Array()
-  // let arr = [1, 2, 3, 4, 5];
-  // arr.map((number,i)=>console.log(number));
+          // let arr=[10,20,30,40,50];
+          // arr.pop();
+          // console.log(arr);
+
+          // let arr = [10,20,30,40,50];
+          // arr.shift();
+          // console.log(arr);
+
+          // let arr =[10,20,30,40,50];
+          // arr.splice(2,1);
+          // console.log(arr);
+
+          // let arr =[10,20,30,40,50];
+          // arr.splice(3,0,50);
+          // console.log(arr);
+
+          // let arr =[10,20,30,40,50];
+          // console.log(arr.toString());
+
+          // let arr =[10,20,30,40,50];
+          // let main = arr.some((a) => a<20);
+          // console.log(main);
 
 
-  // const numbers = [1, 2, 3, 4, 5];
-  // const doubled = numbers.map((number) => number * 2);
-  // console.log(doubled);
-
-//2.concat()
-//   const array1 = ['a', 'b', 'c'];
-//   const array2 = ['d', 'e', 'f'];
-//   const array3 = array1.concat(array2);
-
-// console.log(array3);
-// expected output: Array ["a", "b", "c", "d", "e", "f"]
-
-
-//3.to string()
-// const number = 1212
-// const numberString = number.toString();
-// console.log(number);
-
+          // let arr =[10,20,30,40,50];
+          // let main =arr.find((a) => a>10);
+          // console.log(main);
+        
+          // let arr =[10,"kinu",30,40,50];
+          // let res =Array.isArray(arr);
+          // console.log(res);
 
 
   return (
@@ -73,7 +66,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 123
+         krinali
         </a>
       </header>
     </div>

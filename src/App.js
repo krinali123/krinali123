64b55c679478c329dd12c0f7a807442350d73d1c
krinali123 <paneliyakrinali2@gmail.com>
2022-05-02 11:09:42 +0530

    2-5

--- src/App.js
+++ src/App.js
@@ -46,38 +46,43 @@ const data =
     status: false
   }
 ];
-// data.map((value,index) => console.log(value.id,value.name));
-let filterdata =data.filter((d,i) => d.expiry>=2022);
-console.log(filterdata);
+//  data.map((value,index) => console.log(value.id,value.name));
+ let filterdata =data.filter((data,index) => data.expiry>=2022);
+//  console.log(filterdata);
+  let ans= filterdata.reduce((acc,data,index) => acc+data.price,0);
+//  console.log(ans);
+var meta =data 
+ .filter((data,index) => data.expiry>=2022)
+ .reduce((acc,data,index) => acc+data.price,0);
  
   return (
     <>
    <table>
-  
+        <th>
+          <td>id</td>
+       </th>
+       <th >
+          <td>name</td>
+       </th>
+       <th>
+          <td>price</td>
+       </th>
+       <th>
+           <td>quantity</td>
+       </th>
      <th>
-       <td>id</td>
-       </th>
-       <th>
-       <td>name</td>
-       </th>
-       <th>
-       <td>price</td>
-       </th>
-       <th>
-       <td>quantity</td>
-       </th>
-       <th>
        <td>Total Cost</td>
      </th>
      {
        filterdata.map((value,index) => {
          return(
            <tr>
-             <td>{ value.id}</td>
-             <td>{ value.name}</td>
-             <td>{ value.price}</td>
-             <td>{ value.quantity}</td>
-             filterdata
+             <td>{value.id}</td>
+             <td>{value.name}</td>
+             <td>{value.price}</td>
+             <td>{value.quantity}</td>
+             <td>{meta}</td>
+             
            </tr>
          )
        })

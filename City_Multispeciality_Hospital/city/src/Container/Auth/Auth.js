3a2c653f40829740f9c4d5f83bcb82f0517606f8
krinali123 <paneliyakrinali2@gmail.com>
2022-05-25 10:49:42 +0530

    pro-2 25-5

--- City_Multispeciality_Hospital/city/src/Container/Auth/Auth.js
+++ City_Multispeciality_Hospital/city/src/Container/Auth/Auth.js
@@ -1,8 +1,53 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { useState } from 'react';
+import * as yup from 'yup';
+
 function Auth(props) {
+    const[login,section]=useState('login')
+    let schema = yup.object().shape({
+      
+        email: yup.string().email(),
+       password:yup.string().password()
+        })
+   
     return (
-        <Button color="danger">primary</Button>
+        <div>
+            <section className="appointment" id="appointment">
+                <div className='container'>
+                    {
+                        login==='login'?
+                        <div className="section-title">
+                        <h2>login</h2>
+                        </div>
+                        :
+                        <div className="section-title">
+                        <h2>sign up</h2>
+                        </div>
+
+                    }
+                    
+                  
+            
+                    </div> 
+            <form action method="post" role="form" className="php-email-form">
+                <div className="row">
+                    <div className="col-md-4 form-group">
+                        <input type="text" name="email" className="form-control" id="email" placeholder="Your email" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div className="validate" />
+                    </div>
+                    <div className="col-md-4 form-group">
+                        <input type="text" name="password" className="form-control" id="password" placeholder="Your password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div className="validate" />
+                        <div><button className="text-center" type="button" onClick={() => setlogin('login')}>login</button></div>
+                        <div className="text-center"><button type="button" onClick={() => setlogin('sign in')}>login</button>sign in</button></div>
+                    </div>
+                </div>
+                </form>
+
+                </div>
+                </section>
+                
+
     );
 }
 
---
+++ fruitkha-1.0.0/fruitkha/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./Componets/Footer/Footer";
+import Header from "./Componets/Header/Header";
+
+
+function App() {
+  return (
+  <>
+  <Header/>
+  <Footer/>
+  </>
+  );
+}
+
+export default App;

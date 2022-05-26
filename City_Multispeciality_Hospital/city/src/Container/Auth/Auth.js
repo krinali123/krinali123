7dfcf009232e6f8dde32985d4a755ff634b16af9
paneliyakrinali <paneliyakrinali2@gmail.com>
2022-05-26 09:09:09 +0530

    26-5

--- City_Multispeciality_Hospital/city/src/Container/Auth/Auth.js
+++ City_Multispeciality_Hospital/city/src/Container/Auth/Auth.js
@@ -1,54 +1,108 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import * as yup from 'yup';
+import { Formik, Form, useFormik } from 'formik';
+
 
 function Auth(props) {
-    const[login,section]=useState('login')
-    let schema = yup.object().shape({
-      
-        email: yup.string().email(),
-       password:yup.string().password()
-        })
-   
-    return (
-        <div>
-            <section className="appointment" id="appointment">
-                <div className='container'>
-                    {
-                        login==='login'?
-                        <div className="section-title">
-                        <h2>login</h2>
-                        </div>
-                        :
-                        <div className="section-title">
-                        <h2>sign up</h2>
-                        </div>
 
-                    }
-                    
-                  
-            
-                    </div> 
-            <form action method="post" role="form" className="php-email-form">
+  const [login, setlogin] = useState('login')
+
+  let login1 = {
+
+    email: yup.string().email("plaese enter valid  email ").required("plz enter email id"),
+    password: yup.string().required("enter filed ")
+  }
+
+  let schema = yup.object().shape(login1);
+
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: schema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+   console.log(formik.errors.email);
+
+
+
+
+  return (
+    <>
+      <section id="appointment" className="appointment">
+        <div className="container">
+          {
+            login === 'login' ?
+              <div className="section-title">
+                <h2> login</h2>
+              </div>
+              :
+              <div className="section-title">
+                <h2> sign up </h2>
+              </div>
+          }
+          <Formik value={Formik}>
+
+            <Form onSubmit={formik.handleSubmit} className="php-email-form">
+              {
+                login === 'sign-in' ?
+                  <div className="col-md-4 form-group">
+                    <input type="text"
+                      name="name"
+                      className="form-control"
+                      id="name"
+                      placeholder="Your Name"
+                      data-rule="minlen:4" data-msg="Please enter at least 4 chars"
+                      onChange={formik.handleChange}
+                    />
+
+                    <div className="validate" />
+                  </div>
+                  : null
+              }
+                <div className="col-md-4 form-group mt-3 mt-md-0">
+                  <input type="email"
+                  className="form-control" 
+                  name="email" id="email"
+                   placeholder="Your Email"
+                    data-rule="email" 
+                    onChange={formik.handleChange} />
+                  <div className="validate" />
+                  {
+                    formik.errors.email ? <p> {formik.errors.email}</p> : null
+                  }
+                </div>
                 <div className="row">
-                    <div className="col-md-4 form-group">
-                        <input type="text" name="email" className="form-control" id="email" placeholder="Your email" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                        <div className="validate" />
-                    </div>
-                    <div className="col-md-4 form-group">
-                        <input type="text" name="password" className="form-control" id="password" placeholder="Your password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                        <div className="validate" />
-                        <div><button className="text-center" type="button" onClick={() => setlogin('login')}>login</button></div>
-                        <div className="text-center"><button type="button" onClick={() => setlogin('sign in')}>login</button>sign in</button></div>
-                    </div>
+                <div className="col-md-4 form-group">
+                  <input type="text"
+                   name="password" 
+                   className="form-control" 
+                   id="password"
+                    placeholder="Your password"
+                     data-rule="minlen:4"
+                     onChange={formik.handleChange} />
+                  <div className="validate" />
+                  {
+                    formik.errors.password ? <p> {formik.errors.password}</p> : null
+                  }
                 </div>
-                </form>
 
-                </div>
-                </section>
-                
+                <div><button className="text-center" type="submit" onClick={() => setlogin('login')}>login</button></div>
+                <div className="text-center"><button className="text-center" type="submit" onClick={() => setlogin('sign-in')}>sign-in</button></div>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </section>
 
-    );
+    </>
+  );
 }
+
 
 export default Auth;
